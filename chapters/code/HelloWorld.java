f4360d1ee6e4ca29bf1a2e97acb129e80f394693
Jan Kaßel <jan@kassel.works>
2020-04-01 23:08:40 +0200

    chore: remove legacy code sample

--- chapters/code/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-/**
- *  Hello, world - example in Java.
- */
-public class Main{
-    // says hello to the world
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
-    }
-}
